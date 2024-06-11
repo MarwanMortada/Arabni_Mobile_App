a5d78d7bae4a13399e5d92cd5c1c93bc1300fe25
MarwanMortada <M.Rashwan@nu.edu.eg>
2024-06-12 00:45:23 +0300

    Trial

---
+++ main/lib/core/utils/image_constants.dart
@@ -0,0 +1,6 @@
+abstract class ImageConstants {
+  static const String profileImage = 'assets/images/Profileimage.png';
+  static const String appBarImage = 'assets/images/Arabni_Logo.png';
+
+  static const String illustrationImage = 'assets/images/LoginPic.png';
+}
